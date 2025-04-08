db6cfefeff828e207e79946428b5e53e01946758
furqan759 <muhammadfurqan619@gmail.com>
2025-04-08 18:13:31 +0500

    font family added globally

---
+++ assignment/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['manrope', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
